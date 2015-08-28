fc5b9906e967bb5b99422da4719c8cd28edf3502
field210 <field210@hotmail.com>
2015-08-28 17:33:56 -0400

    add html for 404 base

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask, render_template, request, redirect
+import os
 
+from flask import Flask, render_template, send_from_directory
+
+
+
+# initialization
 app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+)
 
-@app.route('/')
-def main():
-  return redirect('/index')
 
-@app.route('/index')
+# controllers
+@app.route("/")
+def hello():
+    return "Hello from P"
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route("/")
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-if __name__ == '__main__':
-  app.run(port=33507)
+
+# launch
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
