632836ec6d53a3ce125d26d7488213ef04c5ff2f
field210 <field210@hotmail.com>
2015-09-30 11:02:56 -0400

    remove stock and electric page

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-Yuan Tian's The Data Incubator Project
+Housing Sold Price Predictor
 ======================================
 
-Milestone Project
------------------
 
-### Stock Price Checker
-
-This app uses the Quandle WIKI dataset as stock price data source. After inputing the stock ticker, price type and date range, an interactive plot of stock historical price will be shown.
-
-https://yuantian.herokuapp.com/stock/
-
-Capstone Project
-----------------
--- app.py
+++ app.py
@@ -5,9 +5,7 @@ from flask import Flask, render_template, send_from_directory, request, \
 import numpy as np
 import pandas as pd
 from dateutil.relativedelta import relativedelta
-from bokeh import embed
-from bokeh.charts import Line
-import Quandl
+
 import dill
 from pandas.io.json import json_normalize
 
@@ -38,94 +36,9 @@ def page_not_found(e):
 
 
 # render home page
-@app.route("/")
-@app.route("/index/")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index/", methods=["GET", "POST"])
 def index():
-    title = "Yuan's Apps"
-    paragraph = ["This is a collection of my apps.",
-                 "More in development."]
-
-    return render_template("index.html", title=title, paragraph=paragraph)
-
-
-# stock checker
-
-# user function: take user inquiry, query quandl api, make price vs date
-# plot, return bokeh plot
-def plot_stock(ticker, price, duration):
-    # === input example ===
-    # ticker = "goog"
-    # price = ["Low", "High", "Close"]
-    # duration = 12
-
-    quandl_api_key = "vzzyasdeesFi-345MTXg"
-    try:
-        df = Quandl.get("WIKI/" + ticker, authtoken=quandl_api_key)
-    except:
-        return False
-
-    # calculate start date
-    date_end = datetime.today()
-    date_start = date_end + relativedelta(months=-duration)
-
-    # convert datetime.datetime to numpy.datetime64, and compare
-    if df.index.values.min() > np.datetime64(date_start):
-        df_subset = df.loc[:, price]
-    else:
-        df_subset = df.loc[date_start:date_end, price]
-
-    # show different plot title based on duration
-    if duration == 9999:
-        title = "All available stock price for " + ticker
-    else:
-        title = str(duration) + "-month stock price for " + ticker
-
-    # define palette
-    palette = pd.DataFrame(['red', 'green', 'blue'],
-                           index=['Low', 'High', 'Close'], columns=['color'])
-    palette_select = list(palette.loc[price, :].values.flatten())
-
-    # bokeh plot
-    p = Line(df_subset, title=title, xlabel="Date", ylabel="Stock price (USD)",
-             xscale="datetime", legend=True, palette=palette_select)
-
-    # return bokeh plot
-    return p
-
-
-# render stock checker page
-@app.route("/stock/", methods=["GET", "POST"])
-def stock():
-    title = "Stock Price Checker"
-    paragraph = [
-        "This app uses the Quandle WIKI dataset as stock price data source. "
-        "After inputing the stock ticker, price type and date range, "
-        "an interactive plot of stock historical price will be shown."]
-
-    if request.method == "POST":
-        r = request.form
-
-        ticker = r.getlist("ticker")[0].strip()
-        price = r.getlist("price")
-        duration = int(r.getlist("duration")[0])
-
-        # print(ticker, type(ticker), "\n", price, type(price), "\n",
-        # duration, type(duration))
-
-        plot = plot_stock(ticker, price, duration)
-
-        if not plot:
-            return jsonify({})
-        else:
-            script, div = embed.components(plot)
-            return jsonify({"script": script, "div": div})
-    else:
-        return render_template("stock.html", title=title, paragraph=paragraph)
-
-
-# render housing page
-@app.route("/housing/", methods=["GET", "POST"])
-def housing():
     if request.method == "POST":
         r = request.form
 
